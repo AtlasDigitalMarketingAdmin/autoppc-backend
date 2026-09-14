c6f8dd246d8fe4f78d8d900dba31c36b1d19d6b7
AtlasDigitalMarketingAdmin <admin@atlasgrowthdigitalmarketing.com>
2026-09-14 15:25:50 -0400

    Add Netlify domain to CORS

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'https://autoppc-frontend.vercel.app',
+    'https://frabjous-strudel-46555c.netlify.app',
     'https://www.autoppc.io'
   ]
 }));
